f49914bf0e886ef7bb203b85a4ee1fea0a7b5f58
Zatvor <a.zatvornitskiy@gmail.com>
2013-01-03 13:30:00 +0200

    Acceptance test for examples/1-example-scenario.exs

--- examples/1-example-scenario.exs
+++ examples/1-example-scenario.exs
@@ -1,42 +1,39 @@
 use CouchNormalizer.Scenario
 
 CouchNormalizer.Registry.acquire "1-example-scenario", fn(db, _doc_id, _rev, body) ->
+  # 0. retrieves field value from the given document body.
+  # `field/1`, `field/2` just another convenience for getting values from the body.
+  if body["type"] == "user" do
+    # 1. updates/improves document structure.
+    update_field :field, String.upcase(body["field"])
+    rename_field :old_name, :new_name
 
-  # before filter hook
-  if field(body, :type) == "user" do
+    # 2. removes unused/deprecated fields.
+    remove_field  :unused_a
+    remove_fields [:unused_b, :unused_c, :unused_d]
 
-    # 1. updates/improves document structure
-    update_field    :field, String.downcase(field(:b))
-    rename_field    :old_name, :new_name
-
-    # 2. cleanups unused/deprecated fields
-    remove_field    :unused_a
-    remove_fields   [:unused_b, :unused_c, :unused_d]
-
-    # 3. creates new fields
+    # 3. creates new fields.
     create_field :string, "string"
     create_field :array,  ["hello", "world"]
     create_field :hash,   {[{"key", "value"}, {"key_1", "value_1"}]}
-    create_field :number, 10
+    create_field :integer, 10
 
-    # 4. reads external document
+    # 4. reads some value from external document.
     create_field :link, doc_field("ddoc", :link)
-    # 4.1 gets field from another db
-    create_field :link2, doc_field("db", "ddoc", :link)
-
+    # 4.1 reads some doc and value from external db.
+    create_field :link1, doc_field("db", "ddoc", :link)
     # 4.2 reads external document once and cache it.
-    # so further method calls (during particular normalization process) return cached value
-    create_field :link, doc_field(db, "ddoc", :link, :cached!)
+    # so, further method calls (during particular normalization session) returns cached value.
+    create_field :link2, doc_field(db, "ddoc", :link, :cached!)
 
-    # 5. removes current document (mark as _deleted = true)
     if field(body, :no_longer_available) == true do
+      # 5. removes current document (mark as _deleted = true).
       mark_as_deleted!
-
-      # 6. removes some external document
+      # 5.1 removes some external document.
       remove_document!("db", field(:ticket_id))
     end
 
-    # notifies the normalizer engine about changes which should be saved.
+    # Finally, notifies the normalizer engine about changes which should be stored (update document).
     {:update, body}
   end
 end
---
+++ test/example_scenario_test.exs
@@ -0,0 +1,66 @@
+Code.require_file "../test_helper.exs", __FILE__
+
+
+defmodule CouchNormalizer.ExampleScenarioTest do
+  use ExUnit.Case, async: false
+
+  defmodule StubbedCouchDb do
+    def body, do: [ {"link", "example.com"} ]
+    def document_body(_db, _id), do: body
+    def update_doc(_body), do: :done!
+  end
+
+  CouchNormalizer.Scenario.under_test(StubbedCouchDb)
+
+  @subject "examples/1-example-scenario.exs"
+
+  def setup(_) do
+    CouchNormalizer.Registry.init
+    CouchNormalizer.Registry.load(@subject)
+  end
+
+  def subject_fun() do
+    [{ _, _, subject }] = :ets.lookup(CouchNormalizer.Registry.to_ets, "1")
+    subject
+  end
+
+
+  test "does load the #{@subject} scenario" do
+    assert is_function(subject_fun)
+  end
+
+  test "tries to call with improper context" do
+    assert CouchNormalizer.Scenario.call(subject_fun, {"db", "ddoc", "rev", [{"type", "track"}]}) == nil
+  end
+
+  test "updates some 'user' document" do
+    # should be removed
+    removed_fields = [ {"unused_a", "a"}, {"unused_b", "b"}, {"unused_c", "c"}, {"unused_d", "d"}, {"old_name", "name"} ]
+    # should be updated
+    updated_fields = [ {"field", "UPCASE"}, {"new_name", "name"} ]
+    # should be created
+    created_fields = [ {"string", "string"}, {"array",  ["hello", "world"]}, {"hash", {[{"key", "value"}, {"key_1", "value_1"}]}}, {"integer", 10} ]
+    # should be created
+    # created_external_fields = [ {"link", "example.com"}, {"link1", "example.com"}, {"link2", "example.com"} ]
+
+    # should be normalized by @subject scenario
+    document = [ {"type", "user"} ] ++ removed_fields ++ updated_fields ++ created_fields
+    { :update, body } = CouchNormalizer.Scenario.call(subject_fun, {"db", "ddoc", "rev", document})
+    body = HashDict.new body
+
+    # should be removed
+    assert_fields_should_be removed_fields, fn({k,_v}) -> body[k] == :nil end
+    # should be updated
+    assert_fields_should_be updated_fields, fn({k,v})  -> body[k] == v end
+    # should be created
+    assert_fields_should_be created_fields, fn({k,v})  -> body[k] == v end
+    # should be created
+    # assert_fields_should_be created_external_fields, fn({k,v})  -> body[k] == v end
+  end
+
+
+  def assert_fields_should_be(fields, map_fun) do
+    mapped = Enum.map fields, map_fun
+    Enum.each mapped, fn(s) -> assert(s) end
+  end
+end
