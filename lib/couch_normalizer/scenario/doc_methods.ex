5cdd6b453216c4210d950a79f28909320124d943
Zatvor <a.zatvornitskiy@gmail.com>
2013-01-16 17:13:24 +0200

    Fixed, closes #54

--- lib/couch_normalizer/scenario/doc_methods.ex
+++ lib/couch_normalizer/scenario/doc_methods.ex
@@ -50,13 +50,13 @@ defmodule CouchNormalizer.Scenario.DocMethods do
   end
 
   @doc false
-  defmacro mark_as_deleted(body) do
-    quote do: create_field(unquote(body), "_deleted", :true)
+  defmacro mark_as_deleted(not_found) when is_atom(not_found) do
+    quote do: :not_found
   end
 
   @doc false
-  defmacro mark_as_deleted(not_found) when is_atom(not_found) do
-    quote do: :not_found
+  defmacro mark_as_deleted(body) do
+    quote do: create_field(unquote(body), "_deleted", :true)
   end
 
 
