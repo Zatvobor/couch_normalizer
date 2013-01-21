08265184959575f8f2df941a9f64d235e96d5c5f
Zatvor <a.zatvornitskiy@gmail.com>
2013-01-21 13:13:07 +0200

    Return back  for Registry paths

--- lib/couch_normalizer/registry.ex
+++ lib/couch_normalizer/registry.ex
@@ -40,11 +40,11 @@ defmodule CouchNormalizer.Registry do
 
   @doc false
   def load_all(path) do
-    Enum.each Path.wildcard(path <> "/*.exs"), fn(f) -> Code.load_file(f) end
+    Enum.each Path.wildcard(to_binary(path) <> "/*.exs"), fn(f) -> Code.load_file(f) end
   end
 
   @doc false
   def load(file) do
-    file |> Code.load_file
+    to_binary(file) |> Code.load_file
   end
 end
